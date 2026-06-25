b94bfe15011fb04daf48d982744a15b8427b0069
Kinga <167677508+essenatura@users.noreply.github.com>
2026-06-25 15:47:52 +0200

    Delete app directory

--- app/src/main/java/com/essenatura/mouseweather/MouseWeatherWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.essenatura.mouseweather
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class MouseWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = MouseWeatherWidget()
-}
